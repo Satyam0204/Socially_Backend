db3c922736f88b55398e0f812f75777190f8fc25
Satyam0204 <satyampattanaik007@gmail.com>
2022-12-27 22:04:34 +0530

    db for deployment

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 set -o errexit
+cd socialmedia/
 poetry install
+cd ..
+cd socially_backend/
 python manage.py collectstatic --no-input
 python manage.py migrate
--- socially_backend/socially_backend/settings.py
+++ socially_backend/socially_backend/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xmsv@*pbitpgff=ow0r04*_ac5=mi)wmua25nx+sg1@gsdcwg0"
+# SECRET_KEY = "django-insecure-xmsv@*pbitpgff=ow0r04*_ac5=mi)wmua25nx+sg1@gsdcwg0"
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
