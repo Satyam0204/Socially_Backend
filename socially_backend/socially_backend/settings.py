960d0897150d14ad9e7b1ad4eef955520ae00de9
Satyam0204 <satyampattanaik007@gmail.com>
2022-12-28 01:21:32 +0530

    changes in build file

--- socially_backend/socially_backend/settings.py
+++ socially_backend/socially_backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
-import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
