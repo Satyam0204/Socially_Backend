2d0c045467a164aa996afa56fa890b76f7515d51
Satyam0204 <satyampattanaik007@gmail.com>
2022-12-28 01:39:17 +0530

    changes in build file

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ pip install -r ./socially_backend/requirements.txt
 
 python3 ./socially_backend/manage.py collectstatic --no-input
 python3 ./socially_backend/manage.py migrate
+
+cd ./socially_backend/
